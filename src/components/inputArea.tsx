229e6c42be933bd900e581a547d70c13906e578f
Begüm Ircı <begumirci@Begum-MacBook-Air.local>
2024-05-25 16:26:49 +0300

    inputdene

--- src/components/inputArea.tsx
+++ src/components/inputArea.tsx
@@ -1,14 +1,34 @@
-import { TextField } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { Checkbox, TextField, Typography, Stack } from '@mui/material';
 
-type InputProps = 'standard' | 'outlined' | 'contained';
+const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
 
 export const InputArea = () => {
-  const input: InputProps = 'contained';
+  const [inputValue, setInputValue] = useState<string>('');
+
+  useEffect(() => {
+    console.log(inputValue);
+  }, [inputValue]);
 
   return (
-    <>
-      <TextField label='erhan' variant='standard' />
-      {input}
-    </>
+    <Stack>
+      <TextField
+        label='Search Filter'
+        variant='standard'
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <Stack
+        direction='row'
+        display='flex'
+        justifyContent='center'
+        alignItems='center'
+      >
+        <Checkbox {...label} />
+        <Typography variant='body1' component='span'>
+          Only show products in stock
+        </Typography>
+      </Stack>
+    </Stack>
   );
 };
--- src/components/tableWrapper.tsx
+++ src/components/tableWrapper.tsx
@@ -1,3 +1,3 @@
 export const TableWrapper = () => {
-  return <>TableWrapper</>;
+  return <></>;
 };
